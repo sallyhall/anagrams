ca5006a0f5331d4e5c6a247c06ae564cbe5890c1
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-13 14:55:30 -0500

    passes all tests

--- anagram-sample.js
+++ anagram-sample.js
@@ -4,8 +4,18 @@ function anagram( word ) {
 
   return {
     matches: function(subjects) {
-
-
+      if (typeof subjects === "string"){
+        subjects = Array.prototype.slice.call(arguments);
+      }
+      var sortedWord = word.toLowerCase().split("").sort().join("");
+      var anagrams = [];
+      subjects.forEach(function (subject) {
+        var sortedSubject =subject.toLowerCase().split("").sort().join("")
+        if (sortedSubject === sortedWord && word.toLowerCase()!=subject.toLowerCase()){
+          anagrams.push(subject);
+        }
+      });
+      return anagrams;
     }
   };
 
